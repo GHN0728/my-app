5ce34707f60e083895b54f7857b6c2b191a77137
GHN0728 <gim29771@gmail.com>
2026-09-14 15:49:03 +0900

    1st

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Counter } from "@/componunts/Counter";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main>
+      <h1> 웹서버보안프로그래밍</h1>
+      <Counter />
+      <br />
+      <Link href="/about"> /about 페이지로 이동하기 </Link>
+    </main>
+  );
+}
